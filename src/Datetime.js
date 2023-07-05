1d06814868aa1937f86052a109b7ea28b1fec058
Antony Vielo <AntonyVielo4@gmail.com>
2023-07-05 21:53:27 +0700

    com4

--- src/Datetime.js
+++ src/Datetime.js
@@ -6,3 +6,18 @@ var datetime = () => {
 
     var dt = showdate.toDateString();
     var displaytime = showdate.getHours()+'/  Menit : '+showdate.getMinutes()+'/ Detik : '+showdate.getSeconds();
+
+    return(
+        <div>
+            Tanggal Hari Ini : 
+            <input type="text" value={ displaytodaysdate} readOnly="true"/>
+
+            <center>
+            {dt} / Jam : {displaytime}
+            </center>
+        </div>
+    );
+    
+}
+
+export default datetime;
