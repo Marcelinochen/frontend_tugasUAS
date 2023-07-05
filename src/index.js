5af75db684e1d7170542c8e37119de0f46173301
Antony Vielo <AntonyVielo4@gmail.com>
2023-07-05 21:44:25 +0700

    com2

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "bulma/css/bulma.css";
 import Datetime from './Datetime';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Datetime />
+    <App />
+  </React.StrictMode>
+);
