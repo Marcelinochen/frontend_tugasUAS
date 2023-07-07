a43a2551be3b0baadc52b048371e9292be7f94cf
Mario-VC <marioveric2@gmail.com>
2023-07-07 14:19:06 +0700

    commit no5

---
+++ src/AboutUs.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const About = () =>{
+    return (
+        <div className="bg-light">
+            <div className="container py-5">
+                <div className="row align-items-center">
+                    <div className="col-lg-6 d-none d-lg-block"><img src="https://images.app.goo.gl/2vK3ChqmyidKT57h6" alt="" height="50" className="img.fluid" /></div>
+
+                    <div className="col-lg-6 p-5">
+                        <h1 className="display-4">About Us</h1>
+                        <p className="lead mb-0">Hi, <br/> Website perpustakaan yang memiliki berbagai fitur seperti registrasi, penambahan data, pengeditan data dll. Enjoy!!</p>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default About;
